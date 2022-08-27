31c831baa770eb2b4569a04ddb2ecce6d74de41d
Moazzam19 <moazzam@syntaxtechs.com>
2022-08-27 12:28:45 -0400

    Solar system ready to be deployed

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,5 +4,11 @@ public class Main {
 
     public static void main(String[] args) {
 	// write your code here
+        solarSystem inception=new solarSystem();
+        inception.Sun=1;
+        inception.Moon=4;
+        inception.Planets=2;
+
+        System.out.println("the basic structure is ready ");
     }
 }
