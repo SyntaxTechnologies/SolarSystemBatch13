b595da0271a1c1300a517a4fd88c1b1011fcd8a9
Moazzam19 <moazzam@syntaxtechs.com>
2022-08-28 11:15:47 -0400

    feature for planet2 has been added with 0 errors

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,23 +3,28 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
-        solarSystem inception=new solarSystem();
-        inception.Sun=1;
-        inception.Moon=4;
-        inception.Planets=2;
+        // write your code here
+        solarSystem inception = new solarSystem();
+        inception.Sun = 1;
+        inception.Moon = 4;
+        inception.Planets = 2;
 
         System.out.println("the basic structure is ready ");
 
 
 //    developing the feature SUN
-        SUN omaro=new SUN();
-        omaro.temperature="12334433kj";
-        omaro.name="ferro";
-        omaro.radius=1234455;
-        omaro.color="pink";
-    }
+        SUN omaro = new SUN();
+        omaro.temperature = "12334433kj";
+        omaro.name = "ferro";
+        omaro.radius = 1234455;
+        omaro.color = "pink";
 
+//    planet2
+        Planet2 superwomen = new Planet2();
+        superwomen.size="766585857km";
+        superwomen.name="pliea";
+        superwomen.color="green";
+    }
 
 
 }
---
+++ src/com/company/Planet2.java
@@ -0,0 +1,8 @@
+package com.company;
+
+public class Planet2 {
+
+    String color;
+    String name;
+    String size;
+}
