e70ab6edb69d789088fb480c96fa242ac2a33030
Moazzam19 <moazzam@syntaxtechs.com>
2022-08-28 10:53:43 -0400

    planet1 added and is error free

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -18,6 +18,12 @@ public class Main {
         omaro.name="ferro";
         omaro.radius=1234455;
         omaro.color="pink";
+
+//        developing planet1
+        Planet1 superplanet=new Planet1();
+        superplanet.color="magenta";
+        superplanet.name="hash";
+        superplanet.size="545488km";
     }
 
 
